1803e2d177ca8024d4acf29830f311a81e10f864
Abhishek Ahirwar <ahirwarabhi01@gmail.com>
2026-06-29 15:20:16 +0530

    updated indexjs

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 // export const BASE_URL =
 //   Platform.OS === 'android' ? 'http://10.0.2.2:5000' : 'http://localhost:5000';
 
-export const BASE_URL = 'https://shardacrm-backend.onrender.com';
+export const BASE_URL = 'https://shardacrmbe.sharda.co.in';
 
 export const API_BASE_URL = `${BASE_URL}/api/v1`;
 
