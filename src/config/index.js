ad7b2e1855099ef089e795ea475331a16826ced2
sukriti gupta <sukritigupta303@gmail.com>
2026-07-25 11:09:28 +0530

    chances to start app

--- src/config/index.js
+++ src/config/index.js
@@ -6,9 +6,9 @@ global.Buffer = Buffer;
 // export const BASE_URL =
 //   Platform.OS === 'android' ? 'http://10.0.2.2:5000' : 'http://localhost:5000';
 
-export const BASE_URL = 'https://shardacrmbe.sharda.co.in';
+// export const BASE_URL = 'https://shardacrmbe.sharda.co.in';
 
-// export const BASE_URL = 'http://192.168.29.154:5000';
+export const BASE_URL = 'http://192.168.29.154:5000';
 export const API_BASE_URL = `${BASE_URL}/api/v1`;
 
 export const APP_CONFIG = {
